a2e45a03aceafa645e86db5800ed38757f461826
ronen0072 <ronen0072@gmail.com>
2019-09-10 15:16:02 +0300

    fix syllables work well

---
+++ config/keys.js
@@ -0,0 +1,18 @@
+module.exports = {
+    google:{
+        clientID:'457979000681-b337ks9ini6ojrb2dg6qmnc266hdqltk.apps.googleusercontent.com ',
+        clientSecret:'8eSi_Ug41na9GMF_ScAAnnf0'
+    },
+    Oxford:{
+        app_id: '5cd4ffdd', // insert your APP Id
+        app_key: '9bc0c41cf7772885df9f9084e720fc0a' // insert your APP Key
+    },
+    WordsAPI:{
+        'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
+        'x-rapidapi-key': 'nx00f13nfWmshOLjQ67Z1NUKHKGup1YAgKZjsntcXspcZYWgQq'
+    },
+    DictionaryAPI:{
+        apiKey: 'bafe5fa3-9e6b-4e41-b296-04945d3f912d'
+    }
+
+}
