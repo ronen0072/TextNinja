3c5d0afd0daa52a1eeef60a0894a7815fc6b6d47
ronen0072 <ronen0072@gmail.com>
2020-02-20 02:25:54 +0200

    Read form file feature completed

---
+++ client/src/App.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
+import { Provider } from 'react-redux';
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+import queryString from "query-string";
+
+import {loadUser} from "./store/actions/authActions";
+import store from "./store/store";
+import {Header, Footer} from './component/layouts';
+import Home from './component/pages/Home';
+import About from './component/pages/About';
+import Contact from './component/pages/Contact';
+import './App.css';
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#757ce8',
+            main: '#59beee',
+            dark: '#002884',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ff7961',
+            main: '#f44336',
+            dark: '#ba000d',
+            contrastText: '#000',
+        },
+    },
+});
+
+class App extends Component{
+    componentDidMount(){
+        var token = queryString.parse(window.location.search).token;
+        // if(store.getState().auth.isAuthenticated && token)
+        //     window.location.href = '/';
+        // console.log('token: '+token);
+        // console.log('isAuthenticated: ',store.getState().auth.isAuthenticated);
+
+        store.dispatch(loadUser(token));
+
+
+    }
+    render(){
+        return (
+            <Provider store={store}>
+                <BrowserRouter>
+                    <div className="App">
+                        <MuiThemeProvider theme = {theme}>
+                            <Header />
+
+                            <Route exact path='/' component={Home}/>
+                            <Route path='/Home' component={Home}/>
+                            <Route path='/about' component={About}/>
+                            <Route path='/Contact Us' component={Contact} />
+                            <Footer />
+                        </MuiThemeProvider>
+                    </div>
+                </BrowserRouter>
+            </Provider>
+        );
+    }
+}
+
+export default App;
