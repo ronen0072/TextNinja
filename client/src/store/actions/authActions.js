37810f39223ccf9a5a2f43c31928f87d0e0e3e05
ronen0072 <ronen0072@gmail.com>
2020-03-18 00:06:27 +0200

    commit before deploy

--- client/src/store/actions/authActions.js
+++ client/src/store/actions/authActions.js
@@ -94,7 +94,7 @@ export const loginWith =(token) => dispatch => {
                 type: LOGIN_SUCCESS,
                 payload: {user: res.data.user, token},
             });
-            window.history.back();
+            //window.history.back();
         })
         .catch(err => {
             dispatch(returnErrors(err.response.data, err.response.status,));
